e4bb3c1693f75afc4af814514a29fa3c64e1f4b9
Chance Starr <67014622+ChanceStarr@users.noreply.github.com>
2021-05-08 13:38:27 -0400

    Final Reupload Revision Fixes

---
+++ Final/ParticleNet.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+
+def ParticleNet(Train,TrainValues,Test,Epochs):
+    '''Particle Neural Network: Accepts Training Data, Training Values, and Test Data.'''
+
+    class_names = ['Expodential','Normal']
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(5, 1)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(2)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+              
+    model.fit(Train, TrainValues, epochs=Epochs)
+    
+    test_loss, test_acc = model.evaluate(Train,TrainValues)
+
+    print('\nTest accuracy:', test_acc)
+    
+    probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+    
+    predictions = probability_model.predict(Test)
+    
+    return predictions
